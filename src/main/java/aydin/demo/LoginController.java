4ff015476d7fde6ba3820a53f465974b3df445f3
ilknur aydin <aydini@farmingdale.edu>
2024-03-01 22:51:48 -0500

    second commit

--- src/main/java/aydin/demo/LoginController.java
+++ src/main/java/aydin/demo/LoginController.java
@@ -3,9 +3,9 @@ package aydin.demo;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
+
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
