7ac7c512befcaaa1c1bf2dbbcfaddde19e176d63
ilknur aydin <aydini@farmingdale.edu>
2024-03-04 17:09:53 -0500

    demo unidirectional, directional binding and property listener
    features completed: fontsize of the username and password textfield can be updated by moving the slider and entering font size to fontsize text field.
    
    feature completed-signin button remains disabled if username or password is empty

--- src/main/java/aydin/demo/LoginController.java
+++ src/main/java/aydin/demo/LoginController.java
@@ -1,13 +1,19 @@
 package aydin.demo;
 
+import javafx.beans.property.Property;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 
 import javafx.scene.control.Button;
 
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.beans.value.ChangeListener;
+import javafx.util.StringConverter;
 
 import java.io.IOException;
 
@@ -18,11 +24,44 @@ public class LoginController {
     TextField passwordTextField;
     @FXML
     Button signinButton;
+    @FXML
+    Slider fontSizeSlider;
+    @FXML
+    TextField fontSizeTextField;
 
     public void initialize(){
-        // if username or password is empty, signin buttomn is disabled
+
+        //example of adding a unidirectional binding:
+        // if username or password is empty, signin buttom is disabled
         signinButton.disableProperty().bind(
                 usernameTextField.textProperty().isEmpty().or(passwordTextField.textProperty().isEmpty()));
+
+        // example of bidirectional binding:
+        // the fontSize text field and fontSize slider grow or reduce together
+        fontSizeTextField.textProperty().bindBidirectional(fontSizeSlider.valueProperty(), new StringConverter<Number>() {
+            @Override
+            public String toString(Number number) {
+                return number.toString();
+            }
+
+            @Override
+            public Number fromString(String s) {
+                return Double.parseDouble(s);
+            }
+        });
+
+        // example of adding a listener to a property (observable value): value property of the fontSize slider
+        // tahat will cause the text field fonts to change according to the slider value
+        fontSizeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue,
+                                Number oldValue, Number newValue) {
+
+                usernameTextField.setFont(new Font(newValue.doubleValue()));
+                passwordTextField.setFont(new Font(newValue.doubleValue()));
+
+            }
+        });
     }
     @FXML
     protected void onSigninButtonClick() throws IOException {
